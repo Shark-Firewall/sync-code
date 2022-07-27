6c065be1193f815a2ebfe2a59a4385eaa9964be7
Rajnish Kumar <Kumarrajnish363.rk@gmail.com>
2022-07-27 12:39:03 +0530

    50% Editor work Done!

--- frontend/src/Components/Editor.jsx
+++ frontend/src/Components/Editor.jsx
@@ -1,8 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import Client from "./Client";
+import Ide from "./Ide";
 import "./Editor.css";
 
 const Editor = () => {
-  return <div>Editor</div>;
+  const [clients, setClients] = useState([
+    { socketId: 1, username: "Rajnish K" },
+    { socketId: 2, username: "Ankit K" },
+  ]);
+  return (
+    <div className="mainWrap">
+      <div className="aside">
+        <div className="asideInner">
+          <div className="logo">
+            <h1>Code Sync</h1>
+          </div>
+          <h3>Connected</h3>
+          <div className="clientsList">
+            {clients.map((client) => (
+              <Client key={client.socketId} username={client.username} />
+            ))}
+          </div>
+        </div>
+        <button className="btn copyBtn">Copy ROOM ID</button>
+        <button className="btn leaveBtn">Leave</button>
+      </div>
+      <div className="editorwrap">
+        <Ide />
+      </div>
+    </div>
+  );
 };
 
 export default Editor;
---
+++ frontend/src/Components/Ide.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Ide.css";
+
+const Ide = () => {
+  return <div className="container">Ide</div>;
+};
+
+export default Ide;
